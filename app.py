28aa5853f99e9095529600a71fdbcd6ba2ba206a
Miguel Pimenta <miguel.pimenta@Outlook.com>
2025-06-03 23:19:45 +0100

    changes...

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
-from flask import Flask, request, jsonify, render_template, send_from_directory, url_for, abort, jsonify, redirect
+from flask import Flask, request, jsonify, render_template, send_from_directory, url_for, abort, redirect
 from werkzeug.utils import secure_filename
 import requests
 import json
 import os
+import hmac
+from dotenv import load_dotenv
 
-# Flask Restplus https://flask-restplus.readthedocs.io/en/stable/
-# Flask Consulate https://github.com/vsudilov/flask-consulate
+load_dotenv()
 
 app = Flask(__name__)
 
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 app.config['UPLOAD_PATH'] = 'uploads'
-app.config['PLATE_RECOGNIZER_URL'] = os.environ['PLATE_RECOGNIZER_URL']
-app.config['PLATE_RECOGNIZER_TOKEN'] = os.environ['PLATE_RECOGNIZER_TOKEN']
-app.config['API_KEY'] = os.environ['API_KEY']
+app.config['PLATE_RECOGNIZER_URL'] = os.getenv('PLATE_RECOGNIZER_URL')
+app.config['PLATE_RECOGNIZER_TOKEN'] = os.getenv('PLATE_RECOGNIZER_TOKEN')
+app.config['API_KEY'] = os.getenv('API_KEY')
 
 regions = ['pt', 'es'] 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     files = os.listdir(app.config['UPLOAD_PATH'])
     return render_template('index.html', files=files)
 
-@app.route('/uploads/<filename>')
+@app.route('/uploads/<filename>', methods=['GET'])
 def get_image(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 @app.route('/', methods=['POST'])
-def upload_files():   
+def upload_files():  
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400 
     uploaded_file = request.files['file'] 
-    plate = get_plate(uploaded_file)    
-    message = {
-        'plate': plate
-    }
+    plate, error = __get_plate(uploaded_file)
+    if error:
+        return jsonify({'error': error}), 500
     return redirect(url_for('index'))
 
 @app.route("/api/plate", methods=["POST"])
@@ -44,13 +46,14 @@ def upload_image():
         return jsonify({'result': '403 Forbidden'}), 403
     else:        
         uploaded_file = request.files['file'] 
-        plate = get_plate(uploaded_file)    
+        plate = __get_plate(uploaded_file)    
         message = {
             'plate': plate
         }
         return jsonify(message), 200        
 
-def get_plate(uploaded_file):
+###
+def __get_plate(uploaded_file):
     filename = secure_filename(uploaded_file.filename)
     
     if filename != '':
@@ -77,11 +80,11 @@ def get_plate(uploaded_file):
         
         try: 
             os.rename(file_path, os.path.join(app.config['UPLOAD_PATH'], plate.upper() + file_ext))        
-        except:
+        except Exception as e:
             os.remove(os.path.join(app.config['UPLOAD_PATH'], plate.upper() + file_ext))
             os.rename(file_path, os.path.join(app.config['UPLOAD_PATH'], plate.upper() + file_ext))        
           
-        return plate
+        return plate, e
 
 if __name__ == '__main__':    
     app.run(threaded=True, port=5001)    
